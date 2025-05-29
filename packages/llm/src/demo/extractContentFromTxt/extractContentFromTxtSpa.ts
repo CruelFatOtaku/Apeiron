4b66a42d112b8514b810e7e297654271f051fc08
李佳程 <lijiacheng@baicizhan.com>
2025-05-29 14:55:34 +0800

    西班牙语内容处理

--- packages/llm/src/demo/extractContentFromTxt/extractContentFromTxtSpa.ts
+++ packages/llm/src/demo/extractContentFromTxt/extractContentFromTxtSpa.ts
@@ -1,4 +1,3 @@
-import { TransactionIsolationLevel } from "./../../../../../apps/apeironlog/generated/prisma/index.d";
 import fs from "fs";
 import path from "path";
 import { GPTCompletion } from "../../gpt";
@@ -13,11 +12,31 @@ function replaceSChar(txt: string): string {
 
 const replacer = "##txt##";
 const prompt = `我有一组西班牙语单词的释义，请将这个释义整理为json格式
+注意：
+同一个词性放在一个对象里面，例如：
+m. 本质；实体；intr. 是；存在；发生；进行；aux. 被；由
+整理为：
+[
+    {
+      "partOfSpeech": "m.",
+      "translation": "本质；实体；"
+    },
+    {
+      "partOfSpeech": "intr.",
+      "translation": "是；存在；发生；进行；"
+    },
+    {
+      "partOfSpeech": "aux.",
+      "translation": "被；由"
+    }
+  ]
+
+
 格式要求：
 [{"partOfSpeech":"","translation":""}, {"partOfSpeech":"","translation":""}]
 
 
-释义：
+释义如下：
 ${replacer}`;
 function buildPrompt(txt: string): string {
   // Replace the placeholder with the actual text
@@ -134,4 +153,50 @@ async function run() {
   }
 }
 
-run();
+// run();
+
+function saveExcel() {
+  const fileFolder = path.resolve(__dirname, "./target/spanish");
+  if (!fs.existsSync(fileFolder)) {
+    console.error("数据文件夹不存在:", fileFolder);
+    return;
+  }
+  const files = fs
+    .readdirSync(fileFolder)
+    .filter((file) => file.endsWith(".json") && !file.includes("error"));
+  if (files.length === 0) {
+    console.error("没有找到任何json文件");
+    return;
+  }
+  for (const fileIndex in files) {
+    const file = files[fileIndex] as string;
+    const filePath = path.join(fileFolder, file);
+    const excelFileName = file.replace(".json", ".xlsx");
+    const targetFilePath = path.join(fileFolder, excelFileName);
+
+    const content = fs.readFileSync(filePath, "utf-8");
+    const jsonData = JSON.parse(content) as DataType[];
+    const parsedData = jsonData.map((item) => {
+      // 是否是词组
+      const isPhrase = item.word.trim().includes(" ");
+      const translations = item.translations
+        .map((t) =>
+          isPhrase
+            ? `${t.translation}`
+            : `${t.partOfSpeech.trim() ? "[" + t.partOfSpeech + "]" : ""} ${t.translation}`
+        )
+        .join("\n");
+      return {
+        word: item.word,
+        phonetic: item.phonetic,
+        translations,
+      };
+    });
+    const worksheet = xlsx.utils.json_to_sheet(parsedData);
+    const workbook = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    xlsx.writeFile(workbook, targetFilePath);
+    console.log(`已将数据写入 ${targetFilePath}`);
+  }
+}
+saveExcel();
